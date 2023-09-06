a558e44b5504d7ae6d73ed57ad8a6f6ad777c910
Haris Herdiansyah <haris54237@gmail.com>
2023-09-06 09:07:52 +0700

    fix(env): create env for firebase

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,19 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBO7pRl137m_goq1NVBRxUZRKRyRHlyFYM",
-  authDomain: "nextmovie-db.firebaseapp.com",
-  projectId: "nextmovie-db",
-  storageBucket: "nextmovie-db.appspot.com",
-  messagingSenderId: "559183392973",
-  appId: "1:559183392973:web:2044516e125de4976ff984",
-  measurementId: "G-F7TMJZEQY7"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MSG_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 // Initialize Firebase
